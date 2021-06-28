75adb14a0b5b384f138fdf5513b73149521448e0
vaguebrownfox <foxjournal.nsfw@gmail.com>
2021-06-28 13:40:08 +0530

    clean deps

--- src/App.js
+++ src/App.js
@@ -77,6 +77,7 @@ export default function App() {
 				.then(() => {
 					setDevice(pnoi.device);
 					console.log("connected");
+					// handleRssi();
 				})
 				.catch((e) => {
 					console.log(e);
